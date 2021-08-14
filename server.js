8c2c2e043557ec2e50f3abcb8186d023ce43fadd
kandagatla9 <srinathkandagatla6@gmail.com>
2021-08-14 12:05:23 +0530

    final

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const app = require("express")();
-const PORT = process.env.PORT || 3000;
-app.get("", (req, res) => {
-    res.send("Hello world");
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//Server only the static files form the dist directory
+
+app.use(express.static(__dirname+'/dist/charity'));
+
+app.get('/*', function(req,res){
+
+    res.sendFile(path.join(__dirname+'/dist/charity/index.html'));
 });
-app.listen(PORT, () => {
-    console.log(`App up at port ${PORT}`);
-});
+
+//Start the app by listening on the default Heroku port
+
+app.listen(process.env.PORT || 8080);
